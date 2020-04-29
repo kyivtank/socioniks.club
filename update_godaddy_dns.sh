221ab160ec38b12029b29eb3cffc539d115729e3
Oleksandr Liutyi <noc@liutyi.info>
2020-04-29 23:31:09 +0300

    IP fix in godaddy script

--- update_godaddy_dns.sh
+++ update_godaddy_dns.sh
@@ -2,8 +2,7 @@
 echo "Current State Before"
 curl --silent -X GET -H"Authorization: sso-key $GD_API_KEY:$GD_API_SECRET" "https://api.godaddy.com/v1/domains/$GD_DOMAIN/records/A/@"|jq
 
-IPD=`cat terraform/terraform.out |grep PUBLIC_IP |sed 's/PUBLIC_IP = //'`
-IP=`echo $IP` #clean from ANSI colors
+IP=`cat terraform/terraform.out |grep PUBLIC_IP |sed 's/PUBLIC_IP = //'|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"`
 echo "Changing IP to $IP for domain $GD_DOMAIN"
 curl --silent -X PUT "https://api.godaddy.com/v1/domains/$GD_DOMAIN/records/A/@" -H "accept: application/json" -H "Content-Type: application/json" -H "Authorization: sso-key $GD_API_KEY:$GD_API_SECRET" -d "[ { \"data\": \"$IP\", \"port\": 1, \"priority\": 1, \"protocol\": \"string\", \"service\": \"string\", \"ttl\": 3600, \"weight\": 1 }]"
 
