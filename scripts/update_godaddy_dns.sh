9cf75d49f96ed7a818039d5f4231a692755d9e74
Oleksandr Liutyi <noc@liutyi.info>
2020-05-03 02:09:27 +0300

    sleep after DNS update

--- scripts/update_godaddy_dns.sh
+++ scripts/update_godaddy_dns.sh
@@ -1,10 +1,17 @@
 #/bin/bash
 echo "Current State Before"
-curl --silent -X GET -H"Authorization: sso-key $GD_API_KEY:$GD_API_SECRET" "https://api.godaddy.com/v1/domains/$GD_DOMAIN/records/A/@"|jq . --color-output
+curl --silent -X GET -H"Authorization: sso-key $GD_API_KEY:$GD_API_SECRET" "https://api.godaddy.com/v1/domains/$GD_DOMAIN/records/A/@"|jq . --color-output|tee before.txt
 
 IP=`cat terraform/terraform.out |grep PUBLIC_IP |sed 's/PUBLIC_IP = //'|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"`
 echo "Changing IP to $IP for domain $GD_DOMAIN"
 curl --silent -X PUT "https://api.godaddy.com/v1/domains/$GD_DOMAIN/records/A/@" -H "accept: application/json" -H "Content-Type: application/json" -H "Authorization: sso-key $GD_API_KEY:$GD_API_SECRET" -d "[ { \"data\": \"$IP\", \"port\": 1, \"priority\": 1, \"protocol\": \"string\", \"service\": \"string\", \"ttl\": 3600, \"weight\": 1 }]"
 
 echo "Current State After"
-curl --silent -X GET -H"Authorization: sso-key $GD_API_KEY:$GD_API_SECRET" "https://api.godaddy.com/v1/domains/$GD_DOMAIN/records/A/@"|jq . --color-output
+curl --silent -X GET -H"Authorization: sso-key $GD_API_KEY:$GD_API_SECRET" "https://api.godaddy.com/v1/domains/$GD_DOMAIN/records/A/@"|jq . --color-output|tee after.txt
+
+if ! diff after.txt before.txt; 
+ then 
+  echo DNS Updated.. Waiting for 2 minutes
+  sleep 120
+ fi
+
